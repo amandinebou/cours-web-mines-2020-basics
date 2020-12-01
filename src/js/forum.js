8a27543eed46ed64c4d67b96c121fe88a7718c30
amandinebou <amandine.bouguessa1@etu.univ-lorraine.fr>
2020-12-01 15:37:41 +0100

    version statique projet forum

--- src/js/forum.js
+++ src/js/forum.js
@@ -1,21 +1,48 @@
 import ky from "ky";
+import $ from "jquery";
+import { formatDistance } from "date-fns";
+import { fr } from "date-fns/locale";
 
 function getMessageView(message) {
-  return `<div>
-    <p>${message.username}<p/>
-    <p>${message.message}</p>
+  const date = new Date(message.timestamp);
+
+  const dateDistance = formatDistance(date, new Date(), { locale: fr });
+  return `<div class="card my-3">
+    <div class="card-body">
+      <p class="card-text">${message.content}</p>
+    </div>
+      <div class="card-footer text-muted text-right">
+      Par ${message.author}, il y a ${dateDistance}</div>
   </div>`;
 }
 
 function displayMessages(messages) {
-  // Clear list content on view
+  $(".messagesContainer").empty();
   // Iterate on messages and display getMessageView(message);
+  $(".messagesContainer").append(
+    messages.map((message) => getMessageView(message))
+  );
 }
 
 async function refreshMessages() {
+  let pageIndex = 0; // permet modifier valeur comme on veut, peut réassigner valeur
+  let messages = []; // ne peut pas réassigner valeur seulement des éléments
+  let hasMessages = true;
+
+  while (hasMessages) {
+    // eslint-disable-next-line
+    const pageMessages = await ky
+      .get(`https://ensmn.herokuapp.com/messages?page=${pageIndex}`)
+      .json();
+
+    hasMessages = pageMessages.length > 0;
+    pageIndex += 1;
+    messages = messages.concat(pageMessages);
+  }
+
   // GET https://ensmn.herokuapp.com/messages
-  const messages = await ky.get("https://ensmn.herokuapp.com/messages").json();
-  displayMessages(messages);
+
+  displayMessages(messages.reverse());
 }
 
 setInterval(() => {
@@ -23,6 +50,36 @@ setInterval(() => {
 }, 1000);
 
 function sendMessage(message) {
-  // POST https://ensmn.herokuapp.com/messages (username, message)
-  // After success, getMessages()
+  ky.post("https://ensmn.herokuapp.com/messages", { json: message }).then(() =>
+    refreshMessages()
+  );
+  // After success, getMessages();
 }
+
+$("body").on("submit", "#formulaire", (event) => {
+  // stop default form submission
+  event.preventDefault();
+
+  // get form id="formulaire"
+  const $form = $("#formulaire");
+
+  // retrieve data from form
+  const data = $form.serializeArray();
+
+  const messageData = {};
+  data.forEach(({ name, value }) => {
+    messageData[name] = value;
+  });
+
+  if (
+    messageData.username == null ||
+    messageData.message == null ||
+    messageData.username.lenght === 0 ||
+    messageData.message.lenght === 0
+  ) {
+    return;
+  }
+  console.log(messageData);
+  sendMessage(messageData);
+  $("#message").val("");
+});
